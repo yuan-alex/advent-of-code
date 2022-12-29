51177bd1163f0d246cf1caa1683e895feca29547
Alex Yuan <alex@alexyuan.me>
2022-12-29 17:49:20 -0500

    2022/1 part 2 .ts

--- 2022/1/solution.ts
+++ 2022/1/solution.ts
@@ -9,3 +9,11 @@ export function part1(input: string) {
 }
 
 console.log(part1(data));
+
+export function part2(input: string) {
+    const calories = input.split("\r\n\r\n").map(line => line.split("\r\n").map(Number).reduce((a, b) => a + b));
+    calories.sort((a, b) => b - a);
+    return calories[0] + calories[1] + calories[2];
+}
+
+console.log(part2(data));
