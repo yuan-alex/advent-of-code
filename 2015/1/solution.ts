39330d15e24c204308c922ec29b0b795c003bf44
Alex Yuan <alex@alexyuan.me>
2022-12-29 16:50:17 -0500

    2015/1 .ts

---
+++ 2015/1/solution.ts
@@ -0,0 +1,16 @@
+
+import fs from "fs";
+import path from "path";
+
+const data = fs.readFileSync(path.join("./input/input.txt"), "utf8").split("");
+
+let counter = 0;
+data.forEach((i) => {
+    if (i == "(") {
+        counter++;
+    } else {
+        counter--;
+    }
+})
+
+console.log(counter);
