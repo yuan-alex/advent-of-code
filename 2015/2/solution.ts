8095333b0ebd34c7ac246b8bdb775f8592b58206
Alex Yuan <alex@alexyuan.me>
2022-12-29 17:18:17 -0500

    2015/2 part 1

---
+++ 2015/2/solution.ts
@@ -0,0 +1,15 @@
+import fs from "fs";
+import path from "path";
+
+const data = path.resolve("./input.txt", "utf8");
+
+export function part1(input: string) {
+    return input.split(/\r?\n/).map(line => {
+        const [x, y, z] = line.split("x").map(Number);
+        const surfaceArea = 2 * (x * y + x * z + y * z);
+        const smallestSide = Math.min(x * y, x * z, y * z);
+        return surfaceArea + smallestSide;
+    }).reduce((a, b) => a + b, 0);
+}
+
+console.log(part1(data));
