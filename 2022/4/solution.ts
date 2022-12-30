e949abcb4eda0272f470128c295431b42a0e47b5
Alex Yuan <alex@alexyuan.me>
2022-12-29 23:08:01 -0500

    2022/4 part 2 .ts

--- 2022/4/solution.ts
+++ 2022/4/solution.ts
@@ -13,3 +13,11 @@ const part1 = (input: string) =>
   }).length;
 
 console.log(part1(input));
+
+const part2 = (input: string) =>
+  input.split("\r\n").filter((line) => {
+    const [elf1Lo, elf1Hi, elf2Lo, elf2Hi] = line.split(/-|,/).map(Number);
+    return elf1Lo <= elf2Hi && elf2Lo <= elf1Hi;
+  }).length;
+
+console.log(part2(input));
