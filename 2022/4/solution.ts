f2f7fd70cd1d0cccc2b44b1b883edb3f5d399cd0
Alex Yuan <alex@alexyuan.me>
2022-12-29 23:03:15 -0500

    2022/4 part 1 .ts

---
+++ 2022/4/solution.ts
@@ -0,0 +1,15 @@
+import fs from "fs";
+import path from "path";
+
+const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8");
+
+const part1 = (input: string) =>
+  input.split("\r\n").filter((line) => {
+    const [elf1Lo, elf1Hi, elf2Lo, elf2Hi] = line.split(/-|,/).map(Number);
+    return (
+      (elf1Lo <= elf2Lo && elf2Hi <= elf1Hi) ||
+      (elf2Lo <= elf1Lo && elf1Hi <= elf2Hi)
+    );
+  }).length;
+
+console.log(part1(input));
