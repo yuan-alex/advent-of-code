c9743cbc09fe4494cb52911082e435ccdccd41cf
Alex Yuan <alex@alexyuan.me>
2022-12-30 12:44:42 -0500

    2022/6 part 1 .ts

---
+++ 2022/6/solution.ts
@@ -0,0 +1,27 @@
+import { readFileLines } from "../utils/utils";
+import path from "path";
+
+const data = readFileLines(path.resolve(__dirname, "./input.txt"))[0];
+
+const isUnique = (data: string): boolean => {
+  const hashmap = {};
+  for (let i = 0; i < data.length; i++) {
+    const char = data[i];
+    if (hashmap[char]) {
+      return false;
+    } else {
+      hashmap[char] = true;
+    }
+  }
+  return true;
+};
+
+const part1 = (data: string) => {
+  for (let i = 4; i <= data.length; i++) {
+    if (isUnique(data.slice(i - 4, i))) {
+      return i;
+    }
+  }
+};
+
+console.log(part1(data));
